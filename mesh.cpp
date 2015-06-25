1c3bf554ff1401ca4d846985d9621b338d199a30
MrGurken <packo_920@hotmail.com>
2015-06-26 00:14:37 +0200

    Added mesh class.
    The mesh class is responsible for handling information such as a vertex
    buffer object and an index buffer object.  It can also render this
    information.

---
+++ mesh.cpp
@@ -0,0 +1,61 @@
+#include "mesh.h"
+
+Mesh::Mesh()
+	: m_glVAO( 0 ), m_glVBO( 0 ), m_glIBO( 0 ), m_iSize( 0 ), m_glRenderType( GL_TRIANGLES )
+{
+}
+
+Mesh::~Mesh()
+{
+	if( m_glVAO != 0 )
+		glDeleteVertexArrays( 1, &m_glVAO );
+	if( m_glVBO != 0 )
+		glDeleteBuffers( 1, &m_glVBO );
+	if( m_glIBO != 0 )
+		glDeleteBuffers( 1, &m_glIBO );
+}
+
+void Mesh::AddVertices( Vertex* vertices, int nv, GLuint* indices, int ni )
+{
+	// generate a vertex array object
+	glGenVertexArrays( 1, &m_glVAO );
+	glBindVertexArray( m_glVAO );
+
+	// generate a vertex buffer object and send vertex information to it
+	glGenBuffers( 1, &m_glVBO );
+	glBindBuffer( GL_ARRAY_BUFFER, m_glVBO );
+	glBufferData( GL_ARRAY_BUFFER, nv, vertices, GL_STATIC_DRAW );
+	
+	// generate a index buffer object and send index information to it
+	glGenBuffers( 1, &m_glIBO );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, m_glIBO );
+	glBufferData( GL_ELEMENT_ARRAY_BUFFER, ni, indices, GL_STATIC_DRAW );
+
+	glEnableVertexAttribArray( 0 ); // position
+	glEnableVertexAttribArray( 1 ); // texture coordinates
+	//glEnableVertexAttribArray( 2 ); // normal
+	//glEnableVertexAttribArray( 3 ); // tangent
+	//glEnableVertexAttribArray( 4 ); // bitangent
+
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0 );
+	glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(GLfloat)*3) );
+	//glVertexAttribPointer( 2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(GLfloat)*5) );
+	//glVertexAttribPointer( 3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(GLfloat)*8) );
+	//glVertexAttribPointer( 4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(GLfloat)*11) );
+
+	glBindBuffer( GL_ARRAY_BUFFER, 0 );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, 0 );
+	glBindVertexArray( 0 );
+}
+
+void Mesh::Render()
+{
+	glBindVertexArray( m_glVAO );
+	glBindBuffer( GL_ARRAY_BUFFER, m_glVBO );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, m_glIBO );
+
+	glDrawElements( m_glRenderType, m_iSize, GL_UNSIGNED_INT, 0 );
+}
+
+void Mesh::SetRenderType( GLenum renderType ) { m_glRenderType = renderType; }
+GLenum Mesh::GetRenderType() const { return m_glRenderType; }
---
+++ mesh.h
@@ -0,0 +1,35 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "GL\glew.h"
+
+struct Vertex
+{
+	float x, y, z;		// position
+	float u, v;			// texture coordinates
+	//float nx, ny, nz; // normal
+	//float tx, ty, tz; // tangent
+	//float bx, by, bz; // bitangent
+};
+
+class Mesh
+{
+public:
+	Mesh();
+	~Mesh();
+
+	void	AddVertices( Vertex* vertices, int nv, GLuint* indices, int ni );
+	void	Render();
+
+	void	SetRenderType( GLenum renderType );
+	GLenum	GetRenderType() const;
+
+private:
+	GLuint m_glVAO;
+	GLuint m_glVBO;
+	GLuint m_glIBO;
+	GLenum m_glRenderType;
+	int m_iSize;
+};
+
+#endif
