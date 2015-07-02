5961532c814b0011bc489f24f11df9f3050f4a05
MrGurken <packo_920@hotmail.com>
2015-07-02 02:50:22 +0200

    Lua can now register functions to be called
    By calling Refer( functionName ) in Lua, you can register a function to
    be called every frame from the host. Also added the Runtime class that
    holds a vector of Scripts that can now work independantly.

--- Script.cpp
+++ Script.cpp
@@ -1,42 +1,28 @@
 #include "Script.h"
 
-Script::Script()
+Script::Script( lua_State* lua )
+	: m_pLua( lua ), m_bValid( false )
 {
-	m_pLua = luaL_newstate();
-
-	luaL_openlibs( m_pLua );
-	File::lua_Register( m_pLua );
-	GameObject::lua_Register( m_pLua );
-	Texture::lua_Register( m_pLua );
-	Camera::lua_Register( m_pLua );
 }
 
 Script::~Script()
 {
-	lua_close( m_pLua );
-}
-
-Script::operator lua_State*() const
-{
-	return m_pLua;
 }
 
 bool Script::Hotload()
 {
 	bool result = false;
 
-	FileInfo curInfo( m_strFilename );
-	if( curInfo != m_fileInfo )
+	if( m_bValid )
 	{
-		if( curInfo != m_lastFileInfo )
+		FileInfo curInfo( m_strFilename );
+		if( curInfo != m_fileInfo )
 		{
 			printf( "Script.cpp: Hotloading.\n" );
 			Run( m_strFilename );
 
 			result = true;
 		}
-
-		m_lastFileInfo = curInfo;
 	}
 
 	return result;
@@ -51,14 +37,147 @@ bool Script::Run( const string& filename )
 	{
 		printf( "Script.cpp: Lua error: %s\n", lua_tostring( m_pLua, -1 ) );
 		result = false;
+		m_bValid = false;
 	}
 	else
 	{
 		m_strFilename = filename;
-		m_fileInfo.Get( filename );
+		m_bValid = true;
 	}
+
+	m_fileInfo.Get( filename );
 
 	return result;
 }
 
 lua_State* Script::GetState() const { return m_pLua; }
+
+// *******************************************************************
+// Runtime
+// *******************************************************************
+
+Runtime& Runtime::Instance()
+{
+	static Runtime instance;
+	return instance;
+}
+
+Runtime::Runtime()
+{
+	m_pLua = luaL_newstate();
+
+	luaL_openlibs( m_pLua );
+	File::lua_Register( m_pLua );
+	GameObject::lua_Register( m_pLua );
+	Texture::lua_Register( m_pLua );
+	Camera::lua_Register( m_pLua );
+	Maths::lua_Register( m_pLua );
+}
+
+Runtime::~Runtime()
+{
+	lua_close( m_pLua );
+}
+
+Runtime::operator lua_State*() const
+{
+	return m_pLua;
+}
+
+bool Runtime::Hotload()
+{
+	for( script_it it = m_vecScripts.begin(); it != m_vecScripts.end(); it++ )
+		it->Hotload();
+}
+
+bool Runtime::Run( const string& filename )
+{
+	m_vecScripts.push_back( Script( m_pLua ) );
+	script_rit it = m_vecScripts.rbegin();
+	it->Run( filename );
+}
+
+void Runtime::Update()
+{
+	for( ref_it it = m_mapRefs.begin(); it != m_mapRefs.end(); it++ )
+	{
+		lua_rawgeti( m_pLua, LUA_REGISTRYINDEX, it->second );
+		if( lua_pcall( m_pLua, 0, 0, 0 ) )
+		{
+			printf( "Script.cpp: Lua error: %s\n", lua_tostring( m_pLua, -1 ) );
+
+			luaL_unref( m_pLua, LUA_REGISTRYINDEX, it->second );
+			it = m_mapRefs.erase( it );
+		}
+	}
+}
+
+bool Runtime::Refer( const string& func )
+{
+	lua_getglobal( m_pLua, func.c_str() );
+	if( lua_isfunction( m_pLua, -1 ) )
+	{
+		m_mapRefs.insert( pair<string,int>( func, luaL_ref( m_pLua, LUA_REGISTRYINDEX ) ) );
+	}
+	else
+		lua_pop( m_pLua, 1 );
+}
+
+bool Runtime::UnRefer( const string& func )
+{
+	ref_it it = m_mapRefs.find( func );
+	if( it != m_mapRefs.end() )
+	{
+		luaL_unref( m_pLua, LUA_REGISTRYINDEX, it->second );
+		m_mapRefs.erase( it );
+	}
+}
+
+void Runtime::lua_Register( lua_State* lua )
+{
+	lua_register( lua, "Run", lua_Run );
+	lua_register( lua, "Refer", lua_Refer );
+	lua_register( lua, "UnRefer", lua_UnRefer );
+}
+
+int Runtime::lua_Run( lua_State* lua )
+{
+	int result = 0;
+
+	if( lua_gettop( lua ) >= 1 )
+	{
+		const char* script = lua_tostring( lua, 1 );
+		lua_pushboolean( lua, Runtime::Instance().Run( script ) );
+		result = 1;
+	}
+
+	return result;
+}
+
+int Runtime::lua_Refer( lua_State* lua )
+{
+	int result = 0;
+
+	if( lua_gettop( lua ) >= 1 )
+	{
+		const char* func = lua_tostring( lua, 1 );
+		lua_pushboolean( lua, Runtime::Instance().Refer( func ) );
+		result = 1;
+	}
+
+	return result;
+}
+
+int Runtime::lua_UnRefer( lua_State* lua )
+{
+	int result = 0;
+
+	if( lua_gettop( lua ) >= 1 )
+	{
+		const char* func = lua_tostring( lua, 1 );
+		lua_pushboolean( lua, Runtime::Instance().UnRefer( func ) );
+		result = 1;
+	}
+
+	return result;
+}
--- Script.h
+++ Script.h
@@ -2,7 +2,13 @@
 #define SCRIPT_H
 
 #include "lua.hpp"
+#include <vector>
+using std::vector;
+#include <map>
+using std::map;
+using std::pair;
 
+#include "maths.h"
 #include "File.h"
 #include "GameObject.h"
 #include "Texture.h"
@@ -12,10 +18,8 @@
 class Script
 {
 public:
-	Script();
+	Script( lua_State* lua );
 	virtual ~Script();
-
-	operator	lua_State*() const;
 
 	bool		Hotload();
 	bool		Run( const string& filename );
@@ -26,10 +30,46 @@ private:
 	Script( const Script& ref ){}
 	Script& operator=( const Script& ref ){ return *this; }
 
+	bool		m_bValid;
 	string		m_strFilename;
 	FileInfo	m_fileInfo;
-	FileInfo	m_lastFileInfo;
 	lua_State*	m_pLua;
+};
+
+class Runtime
+{
+public:
+	static Runtime& Instance();
+	virtual ~Runtime();
+
+	operator	lua_State*() const;
+
+	bool		Hotload();
+	bool		Run( const string& filename );
+	bool		Refer( const string& func );
+	bool		UnRefer( const string& func );
+
+	void		Update();
+
+	lua_State*	GetState() const;
+
+	static void	lua_Register( lua_State* lua );
+	static int	lua_Run( lua_State* lua );
+	static int	lua_Refer( lua_State* lua );
+	static int	lua_UnRefer( lua_State* lua );
+
+private:
+	Runtime();
+	Runtime( const Runtime& ref );
+	Runtime& operator=( const Runtime& ref ){ return *this; }
+
+	typedef map<string,int>::iterator ref_it;
+	typedef vector<Script>::iterator script_it;
+	typedef vector<Script>::reverse_iterator script_rit;
+
+	lua_State*		m_pLua;
+	map<string,int>	m_mapRefs;
+	vector<Script>	m_vecScripts;
 };
 
 #endif
